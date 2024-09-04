0af16993bfee01edd8b794b195e8cd0da6d84bd9
Johnnie Gonzalez <johnnie.agonz71@gmail.com>
2024-09-04 15:39:05 -0400

    updated forms model, since unqiue is default to false already

--- fastapi/api/schemas.py
+++ fastapi/api/schemas.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel, EmailStr
 from typing import List, Optional
 from datetime import date
 
-# Schema for creating a user
+## Schema for a user
 class UserBase(BaseModel):
     email: EmailStr
     about: Optional[str] = None
@@ -17,4 +17,33 @@ class UserCreate(UserBase):
 
 class User(UserBase):
     id: int
-    email: str
+
+    class Config:
+        from_attributes = True
+
+class UserUpdate(UserBase):
+    email: Optional[str] = None
+    about: Optional[str] = None
+    birthday: Optional[date] = None
+    street_address: Optional[str] = None
+    city: Optional[str] = None
+    state: Optional[str] = None
+    zip_code: Optional[str] = None
+
+## Schema for forms
+class FormBase(BaseModel):
+    name: str
+    page: int
+
+class FormCreate(FormBase):
+    pass
+
+class FormUpdate(FormBase):
+    name: Optional[str] = None
+    page: Optional[int] = None
+
+class Form(FormBase):
+    id: int
+
+    class Config:
+        from_attributes = True

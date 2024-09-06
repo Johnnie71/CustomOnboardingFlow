0fe1dd722f43c84f165bc9ec605aa1b9601751fd
Johnnie Gonzalez <johnnie.agonz71@gmail.com>
2024-09-06 11:19:30 -0400

    removed print statement

--- fastapi/api/routers/users.py
+++ fastapi/api/routers/users.py
@@ -8,7 +8,6 @@ router = APIRouter()
 
 @router.post("/", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session=Depends(get_db)):
-  print(user)
   db_user = crud.get_user_by_email(db, email=user.email)
   if db_user:
     raise HTTPException(status_code=400, detail='Email already registered')
